c69f4dbd368dfa4cbbc9b4fdf60900e96056c229
Ulises Garcia <dev@pixeleate.com>
2019-05-25 23:41:18 -0500

    Adding a console log

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const { router, post } = require("microrouter");
 const { json, send } = require("micro");
 
 const email = async (req, res) => {
+  console.log('Hello NOW');
   const body = await json(req);
   send(res, 200, body);
 };
