5bf7703809d29c3d1e854eb397038985b104a1e8
Ulises Garcia <dev@pixeleate.com>
2019-05-26 03:25:21 -0500

    CORS enabled

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 const { router, post } = require("microrouter");
 const { json, send } = require("micro");
-const cors = require('micro-cors')()
+const microCors = require('micro-cors');
+const cors = microCors({ allowMethods: ['PUT', 'POST'] })
 
 const email = async (req, res) => {
-  console.log('Hello NOW');
   const body = await json(req);
   send(res, 200, body);
 };
 
-const handler = router(post("/email", email));
+const handler = cors(email)
 
-module.exports = cors(handler);
+module.exports = router(post("/email", handler));
